e8b480f40c6a2e68ec180256b707d49d33f8e2c7
in <Pickle423423@gmail.com>
2024-07-16 23:53:25 -0400

    welcomemessageupdated

--- cogs/welcomeMessage.py
+++ cogs/welcomeMessage.py
@@ -14,7 +14,7 @@ class WelcomeMessage(commands.Cog):
         server = member.guild
         sag = server.get_member(68019210814500864)
         ryder = server.get_member(397573639785938945)
-        await channel.send(f"{member.mention}, welcome to [S.E.S.O.] Casual Milsim. \n \n See Channels & Roles to choose your roles.  \n \nIf you are new to Arma 3, see the New Operative Guide to get started: \nhttps://wiki.sesounit.org/guides/new_operative")
+        await channel.send(f"Yo!")
 
 def setup(client):
     client.add_cog(WelcomeMessage(client))
